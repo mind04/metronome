d7941bb6c4307df78e8022ddc8971667c3e57068
Kees Monshouwer <mind04@monshouwer.org>
2024-12-03 23:03:39 +0100

    stop using the now deprecated and useless std::binary_function

--- iputils.hh
+++ iputils.hh
@@ -89,7 +89,7 @@ union ComboAddress {
       return memcmp(&sin6.sin6_addr.s6_addr, &rhs.sin6.sin6_addr.s6_addr, 16) > 0;
   }
 
-  struct addressOnlyLessThan: public std::binary_function<string, string, bool>
+  struct addressOnlyLessThan
   {
     bool operator()(const ComboAddress& a, const ComboAddress& b) const
     {
